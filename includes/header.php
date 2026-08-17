85966128682ef49721617519d99db2bdf6ee1e7e
MikiLeaks <milance.physics@gmail.com>
2026-08-17 16:27:49 +0200

    Dodat i tag manager

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,14 @@ $puni_naslov    = $page_title === $site['naziv'] ? $page_title : $page_title . '
 <!doctype html>
 <html lang="<?= e($site['jezik']) ?>">
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NNFZHCTF');</script>
+<!-- End Google Tag Manager -->
+
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-QXE6F0B3FX"></script>
 <script>
@@ -106,6 +114,10 @@ $puni_naslov    = $page_title === $site['naziv'] ? $page_title : $page_title . '
 <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNFZHCTF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <div class="topbar">
   <div class="container topbar-inner">
